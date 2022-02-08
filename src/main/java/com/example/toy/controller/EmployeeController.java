791fee141f5d884344da848a19e868cc5b3c4822
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-02-08 23:10:04 +0900

    first

---
+++ src/main/java/com/example/toy/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.toy.controller;
+
+import com.example.toy.model.Employee;
+import com.example.toy.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
